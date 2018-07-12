97dcff0b554c65c6dfa119a161f3d05d1dc799c0
Sid He <xhe518@gmail.com>
2018-07-12 20:03:07 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,5 +1,5 @@
 def prime?(integer)
-  (2..integer - 1).each {|x| return false if (integer % x) == 0}
   return false if integer < 2
+  (2..integer - 1).each {|x| return false if (integer % x) == 0}
   true
 end
